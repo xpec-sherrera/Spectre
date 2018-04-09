573eb622561380f021be92ca27f7a4a77cbec68f
xpec-sherrera <shrrera@formax.cl>
2018-04-09 09:06:23 -0500

    Header structure.

--- spectre-front/src/app/app.module.ts
+++ spectre-front/src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+{ path: '',component: HomeComponent, pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
